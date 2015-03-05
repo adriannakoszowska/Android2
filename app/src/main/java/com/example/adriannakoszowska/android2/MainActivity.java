fd449fd50ffc6c3dfe78c8b5b4190e02943daab6
Adrianna Koszowska <adriannakoszowska@gmail.com>
2015-03-05 19:23:29 +0100

    Dodanie layoutu rozmiar

--- app/src/main/java/com/example/adriannakoszowska/android2/MainActivity.java
+++ app/src/main/java/com/example/adriannakoszowska/android2/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.paint_main);
         paintView = (PaintView) findViewById(R.id.view);
         ctx = getApplicationContext();
     }
