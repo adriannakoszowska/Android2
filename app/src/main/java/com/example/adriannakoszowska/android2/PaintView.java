c8a1bcdd0ee7e2c8117be2fd8897da6209d13d7b
Adrianna Koszowska <adriannakoszowska@gmail.com>
2015-03-05 18:05:18 +0100

    stworzenie onTouchEvent

--- app/src/main/java/com/example/adriannakoszowska/android2/PaintView.java
+++ app/src/main/java/com/example/adriannakoszowska/android2/PaintView.java
@@ -2,12 +2,18 @@ package com.example.adriannakoszowska.android2;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
+
+    ArrayList<RectF> punkty;
 
     public PaintView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -35,9 +41,12 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceDestroyed(SurfaceHolder arg0) {
         // TODO Auto-generated method stub
     }
-
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        RectF oval = new RectF(event.getX()-50, event.getY()-50, event.getX() + 50, event.getY() + 50);
+        punkty.add(oval);
+        invalidate();
+
+        return true;
     }
 
     protected void onDraw(Canvas canvas) {
